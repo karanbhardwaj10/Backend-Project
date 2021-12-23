ba22bc1b199a239e8533520a109f1c82da8cebe3
karan bhardwaj <karan.bhardwaj.7917@gmail.com>
2021-12-23 15:37:55 +0530

    added google auth

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 7000;
+const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
@@ -26,6 +27,8 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
+// make the uploads path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
